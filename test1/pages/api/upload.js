73c758dd7a874803396c5eda1b644da1e1fd2c90
Deni Alpiana <Denzalv01@gmail.com>
2022-05-25 08:34:23 +0700

    initial commit

---
+++ test1/pages/api/upload.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { Storage } from '@google-cloud/storage';
+
+export default async function handler(req, res) {
+  const storage = new Storage({
+    projectId: process.env.PROJECT_ID,
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY,
+    },
+  });
+
+  const bucket = storage.bucket(process.env.BUCKET_NAME);
+  const file = bucket.file(req.query.file);
+  const options = {
+    expires: Date.now() + 1 * 60 * 1000, //  1 minute,
+    fields: { 'x-goog-meta-test': 'data' },
+  };
+
+  const [response] = await file.generateSignedPostPolicyV4(options);
+  res.status(200).json(response);
+}
